567eb684d889c04e0b147adb20399875be468a1b
peterokwara <peter.okwara@outlook.com>
2018-10-04 05:07:25 +0300

    Initial commit

---
+++ app/src/main/java/com/example/peter/learnretrofit/remote/APIService.java
@@ -0,0 +1,15 @@
+package com.example.peter.learnretrofit.remote;
+import com.example.peter.learnretrofit.model.Post;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface APIService {
+    @POST("/posts")
+    @FormUrlEncoded
+    Call<Post> savePost(@Field("title") String title,
+                        @Field("body") String body,
+                        @Field("userId") long userId);
+}
---
+++ app/src/main/java/com/example/peter/learnretrofit/remote/ApiUtils.java
@@ -0,0 +1,12 @@
+package com.example.peter.learnretrofit.remote;
+
+public class ApiUtils {
+    private ApiUtils() {}
+
+    public static final String BASE_URL = "http://jsonplaceholder.typicode.com/";
+
+    public static APIService getAPIService() {
+
+        return RetrofitClient.getClient(BASE_URL).create(APIService.class);
+    }
+}
